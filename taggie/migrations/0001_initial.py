5974f0e4d44b2c2f8209fed31b8557dc2cb6d851
Chaitanya Andhare <chaitanya@hike.in>
2015-08-05 16:52:00 +0530

    Database changes. (Reloaded data from current console db)

--- taggie/migrations/0001_initial.py
+++ taggie/migrations/0001_initial.py
@@ -11,28 +11,61 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Sticker',
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=25)),
+                ('rating', models.IntegerField(default=0)),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name=b'date added')),
+                ('upvotes', models.IntegerField(default=0)),
+                ('downvotes', models.IntegerField(default=0)),
+                ('banner', models.CharField(default=None, max_length=150)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='LangType',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=20)),
-                ('category', models.CharField(max_length=20)),
-                ('like', models.IntegerField()),
-                ('rating', models.IntegerField(default=0)),
             ],
-            options={
-            },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Sticker',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=40)),
+                ('category', models.CharField(max_length=20)),
+                ('like', models.IntegerField(default=0)),
+                ('rating', models.IntegerField(default=0)),
+                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name=b'date added')),
+                ('time', models.IntegerField(default=-1)),
+            ],
         ),
         migrations.CreateModel(
             name='Tag',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=20)),
-                ('votes', models.IntegerField()),
+                ('name', models.CharField(max_length=100)),
+                ('upvotes', models.IntegerField(default=0)),
+                ('downvotes', models.IntegerField(default=0)),
+                ('tagtype', models.CharField(max_length=20)),
+                ('lang', models.CharField(default=b'english', max_length=20)),
                 ('sticker', models.ForeignKey(to='taggie.Sticker')),
             ],
-            options={
-            },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TagType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='TypesToTag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+                ('tag', models.ForeignKey(to='taggie.Tag')),
+            ],
         ),
     ]
