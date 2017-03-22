698edc12d9c3f9ea94bb2168d108742746180320
Hitesh Sharma <hitesh@hike.in>
2017-03-22 17:57:46 +0530

    removing utils from development branch

--- StickerConsole/settings.py
+++ StickerConsole/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'sticker_console.urls'
+ROOT_URLCONF = 'StickerConsole.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'sticker_console.wsgi.application'
+WSGI_APPLICATION = 'StickerConsole.wsgi.application'
 
 
 # Database
